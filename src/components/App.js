04a5877ba75fa885535f89d74b722478faf30263
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-07-21 08:06:48 +0300

    Finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [dogPics, setDogPics] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((resp) => resp.json())
+            .then((data) => {
+                setDogPics(data.message)
+            });
+        }, []);
+
+        if (!dogPics) return <p>Loading...</p>;
+
+    return <img src={dogPics} alt="A Random Dog" />;
+}
+
+export default App;
